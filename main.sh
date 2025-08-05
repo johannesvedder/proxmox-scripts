f078af4049f277a3eb3cdade8eb19d613a8f1c54
Johannes Vedder <hallo@johannesvedder.de>
2025-08-05 12:49:11 +0200

    Enhance config file handling in config_parser.sh and main.sh with improved logging and user confirmation

--- main.sh
+++ main.sh
@@ -21,7 +21,7 @@ GITHUB_USER="johannesvedder"
 GITHUB_REPO="proxmox-scripts"
 BRANCH="main"
 ROOT_DIR="/opt/${GITHUB_REPO}"
-CONFIG_FILE="config.sh"  # This is relative to $ROOT_DIR
+CONFIG_FILE="config.sh"
 
 # Get latest remote commit SHA
 REMOTE_COMMIT_SHA=$(curl -s "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/commits/$BRANCH" | jq -r '.sha')
