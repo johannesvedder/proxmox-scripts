59affd96eec896284b3e37b20c7746378aae7ec1
Johannes Vedder <hallo@johannesvedder.de>
2025-08-05 11:01:37 +0200

    Enhance firewall setup in main.sh to specify public bridge for HTTP and HTTPS traffic forwarding

--- repository/apps/nginx_proxymanager/main.sh
+++ repository/apps/nginx_proxymanager/main.sh
@@ -19,19 +19,20 @@ if [[ "$forward_ports" =~ ^[Yy]$ ]]; then
   # Enable IP forwarding
   enable_ip_forwarding
 
-  echo "Setting up firewall rules to forward HTTP and HTTPS traffic..."
+  echo "Setting up firewall rules to forward all incoming HTTP (80) and HTTPS (443) traffic (${PUBLIC_BRIDGE}) to the Nginx Proxy Manager container (${CONTAINER_IP})."
 
-  if ! iptables -t nat -C PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${CONTAINER_IP}:80 2>/dev/null; then
+  if ! iptables -t nat -C PREROUTING -i "${PUBLIC_BRIDGE}" -p tcp --dport 80 -j DNAT --to-destination ${CONTAINER_IP}:80 2>/dev/null; then
     # Forward HTTP
-    iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination ${CONTAINER_IP}:80
+    iptables -t nat -A PREROUTING -i "${PUBLIC_BRIDGE}" -p tcp --dport 80 -j DNAT --to-destination ${CONTAINER_IP}:80
   fi
-  if ! iptables -t nat -C PREROUTING -p tcp --dport 443 -j DNAT --to-destination ${CONTAINER_IP}:443 2>/dev/null; then
+  if ! iptables -t nat -C PREROUTING -i "${PUBLIC_BRIDGE}" -p tcp --dport 443 -j DNAT --to-destination ${CONTAINER_IP}:443 2>/dev/null; then
     # Forward HTTPS
-    iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination ${CONTAINER_IP}:443
+    iptables -t nat -A PREROUTING -i "${PUBLIC_BRIDGE}" -p tcp --dport 443 -j DNAT --to-destination ${CONTAINER_IP}:443
   fi
 
   if [ -n "${INTERNAL_SUBNET}" ]; then
     if ! iptables -t nat -C POSTROUTING -s "${INTERNAL_SUBNET}" -o "${PUBLIC_BRIDGE}" -j MASQUERADE 2>/dev/null; then
+      echo "Configuring NAT MASQUERADE on ${PUBLIC_BRIDGE} for ${INTERNAL_SUBNET}"
       # Allow traffic to leave
       iptables -t nat -A POSTROUTING -s "${INTERNAL_SUBNET}" -o "${PUBLIC_BRIDGE}" -j MASQUERADE
     fi
