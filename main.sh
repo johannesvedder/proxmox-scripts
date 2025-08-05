997d53a71d7449ce469a904f582c563e8df21d04
Johannes Vedder <hallo@johannesvedder.de>
2025-08-05 12:26:57 +0200

    Remove test echo statement and fix conditional block in main.sh

--- main.sh
+++ main.sh
@@ -22,7 +22,6 @@ GITHUB_REPO="proxmox-scripts"
 BRANCH="main"
 ROOT_DIR="/opt/${GITHUB_REPO}"
 CONFIG_FILE="config.sh"
-echo "test"
 
 # Get latest remote commit SHA
 REMOTE_COMMIT_SHA=$(curl -s "https://api.github.com/repos/$GITHUB_USER/$GITHUB_REPO/commits/$BRANCH" | jq -r '.sha')
@@ -69,7 +68,7 @@ else
     update_config "LATEST_COMMIT_SHA" "$REMOTE_COMMIT_SHA"
   # else
     # echo "✅ Repo is already up to date."
-  # fi
+  fi
 fi
 
 export ROOT_DIR
